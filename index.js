6856cce0b2918851031ee2dd3f0efec3f8a43c1f
rodolpop <garciasena@yahoo.com>
2023-08-15 20:06:55 +0200

    js

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const kits= [
+    "crash", "kick", "snare", "tom"
+];
+
+const containerE1 = 
+document.querySelector(".container");
+
+kits.forEach(kit=>{
+     const btnE1 = document.createElement("button");
+     btnE1.classList.add("btn")
+     btnE1.innerText = kit
+     containerE1.appendChild(btnE1);
+     const audioE1 = document.createElement("audio");
+     audioE1.src = "sounds/" + kit + ".mp3"
+     containerE1.appendChild(audioE1);
+});
