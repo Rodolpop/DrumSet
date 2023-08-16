799ae708d83c652574c7cfba2a08074d0b3642bd
rodolpop <garciasena@yahoo.com>
2023-08-16 08:39:46 +0200

    js changes

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
 
-const kits= [
-    "crash", "kick", "snare", "tom"
-];
+const kits = ["crash", "kick", "snare", "tom"];
 
-const containerE1 = 
-document.querySelector(".container");
+const containerE1 = document.querySelector(".container");
 
-kits.forEach(kit=>{
+kits.forEach((kit) => {
+
      const btnE1 = document.createElement("button");
-     btnE1.classList.add("btn")
-     btnE1.innerText = kit
-     btnE1.style.backgroundImage = "url (images/" + kit + " .png)"
+
+     btnE1.classList.add("btn");
+
+     btnE1.innerText = kit;
+
+     btnE1.style.backgroundImage = "url (img/" + kit + ".jpg)";
+
      containerE1.appendChild(btnE1);
+
      const audioE1 = document.createElement("audio");
-     audioE1.src = "sounds/" + kit + ".mp3"
+
+     audioE1.src = "sounds/" + kit + ".mp3";
+
      containerE1.appendChild(audioE1);
-     btnE1.addEventListener("click",()=>{ audioE1.play(); });
-     window.addEventListener("keydown", (event)=>{if (event.key === kit.slice(0,1)){ 
+
+     btnE1.addEventListener("click",() => { 
+        audioE1.play(); 
+    });
+
+     window.addEventListener("keydown", (event) => {
+        if (event.key === kit.slice(0,1)) { 
         audioE1.play();
-        btnE1.style.transform = 
-        "scale(.9)";
-        setTimeout(()=>{
+        btnE1.style.transform = "scale(.9)";
+        setTimeout(() => {
             btnE1.style.transform = "scale (1)";
 
-        },100)
+         },100);
 
         }
 
-     })
+     });
 });
